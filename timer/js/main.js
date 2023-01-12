8deae519244d90bf7b3b4866a8c7c7bd6eed6dad
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-01-12 17:53:10 +0900

    feat: localStorage에 총 공부 시간 저장 및 불러오기

--- timer/js/main.js
+++ timer/js/main.js
@@ -2,9 +2,20 @@
 // play 버튼 누르면 시작, pause 버튼 누르면 일시정지
 const playBtn = document.querySelector(".playBtn");
 const pauseBtn = document.querySelector(".pauseBtn");
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
+let total = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+};
+
+// 창 켜질 때 localStorage에 저장된 값 있으면 불러오기
+window.onload = function () {
+  const loadTotal = localStorage.getItem("total");
+  loadTotal ? (total = JSON.parse(loadTotal)) : total;
+
+  setTotalText();
+};
+
 let startTimer;
 
 playBtn.addEventListener("click", function () {
@@ -12,33 +23,55 @@ playBtn.addEventListener("click", function () {
   pauseBtn.classList.add("active");
   startTimer = setInterval(timer, 1000);
 });
-pauseBtn.addEventListener("click", function () {
+pauseBtn.addEventListener("click", function (e) {
+  e.preventDefault();
   pauseBtn.classList.remove("active");
   playBtn.classList.add("active");
   clearInterval(startTimer);
+  localStorage.setItem("total", JSON.stringify(total));
 });
 
 const timer = function () {
-  const totalH = document.querySelector(".total-stopwatch .hour");
-  const totalM = document.querySelector(".total-stopwatch .minute");
-  const totalS = document.querySelector(".total-stopwatch .second");
+  countUp();
+  setTotalText();
+};
 
+const countUp = function () {
   // 1초에 한번씩 seconds+=1
   // seconds+=1 값이 60일 시 minutes+=1 하고 seconds = 0
   // minutes+=1 값이 60일 시 hours+=1 하고 minutes = 0
+  let {
+    hours: totalHours,
+    minutes: totalMinutes,
+    seconds: totalSeconds,
+  } = total;
+  totalSeconds += 1;
 
-  seconds += 1;
-  if (seconds === 60) {
-    minutes += 1;
-    seconds = 0;
+  if (totalSeconds === 60) {
+    totalMinutes += 1;
+    totalSeconds = 0;
   }
 
-  if (minutes === 60) {
-    hours += 1;
-    minutes = 0;
+  if (totalMinutes === 60) {
+    totalHours += 1;
+    totalMinutes = 0;
   }
-
-  totalH.textContent = hours < 10 ? `0${hours}` : hours;
-  totalM.textContent = minutes < 10 ? `0${minutes}` : minutes;
-  totalS.textContent = seconds < 10 ? `0${seconds}` : seconds;
+  return (total = {
+    hours: totalHours,
+    minutes: totalMinutes,
+    seconds: totalSeconds,
+  });
+};
+const setTotalText = function () {
+  let {
+    hours: totalHours,
+    minutes: totalMinutes,
+    seconds: totalSeconds,
+  } = total;
+  const totalH = document.querySelector(".total-stopwatch .hour");
+  const totalM = document.querySelector(".total-stopwatch .minute");
+  const totalS = document.querySelector(".total-stopwatch .second");
+  totalH.textContent = totalHours < 10 ? `0${totalHours}` : totalHours;
+  totalM.textContent = totalMinutes < 10 ? `0${totalMinutes}` : totalMinutes;
+  totalS.textContent = totalSeconds < 10 ? `0${totalSeconds}` : totalSeconds;
 };
