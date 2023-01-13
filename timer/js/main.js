068d63f949bcf46bbaeb666a76e25dad2edb805b
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-01-13 18:11:53 +0900

    feat: 10초 내로 재시작 안하면 집중 시간 초기화

--- timer/js/main.js
+++ timer/js/main.js
@@ -8,6 +8,12 @@ let total = {
   seconds: 0,
 };
 
+let focusedTime = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+};
+
 let basicSubject = {
   hours: 0,
   minutes: 0,
@@ -32,27 +38,60 @@ window.onload = async function () {
   if (loadSubjects) setSubjectList(JSON.parse(loadSubjects));
 };
 
-let startTimer;
+let startTimer, focused;
+const modal = document.querySelector("#modal");
+let focusModal = document.querySelector(".focus-modal");
+
+// 플레이 버튼 누르면 타이머 시작
+// 쉬는 시간 10초 지나면 현재 집중 시간 리셋
+let count = 10;
 
 playBtn.addEventListener("click", function () {
   playBtn.classList.remove("active");
   pauseBtn.classList.add("active");
   startTimer = setInterval(timer, 1000);
+  clearInterval(focused);
 });
+
+// 일시정지 버튼 누르면 타이머 중단, 집중 모달 등장
+
 pauseBtn.addEventListener("click", function (e) {
   e.preventDefault();
   pauseBtn.classList.remove("active");
   playBtn.classList.add("active");
   clearInterval(startTimer);
   localStorage.setItem("total", JSON.stringify(total));
+  modal.style.display = "block";
+  focusModal.classList.add("active");
+  focused = setInterval(countFocus, 1000);
 });
 
 const timer = function () {
-  countUp();
+  totalCountUp();
   setTotalText();
+  focusCountUp();
+  setFocusText();
 };
 
-const countUp = function () {
+const countFocus = function () {
+  count -= 1;
+  let countText = document.querySelector(".remaining-count");
+  countText.innerText = count;
+
+  if (count === 0) {
+    clearInterval(focused);
+    modal.style.display = "none";
+    focusModal.classList.remove("active");
+    focusedTime = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+    setFocusText();
+  }
+};
+
+const totalCountUp = function () {
   // 1초에 한번씩 seconds+=1
   // seconds+=1 값이 60일 시 minutes+=1 하고 seconds = 0
   // minutes+=1 값이 60일 시 hours+=1 하고 minutes = 0
@@ -91,12 +130,52 @@ const setTotalText = function () {
   totalM.textContent = totalMinutes < 10 ? `0${totalMinutes}` : totalMinutes;
   totalS.textContent = totalSeconds < 10 ? `0${totalSeconds}` : totalSeconds;
 };
+const focusCountUp = function () {
+  // 1초에 한번씩 seconds+=1
+  // seconds+=1 값이 60일 시 minutes+=1 하고 seconds = 0
+  // minutes+=1 값이 60일 시 hours+=1 하고 minutes = 0
+  let {
+    hours: totalHours,
+    minutes: totalMinutes,
+    seconds: totalSeconds,
+  } = focusedTime;
+  totalSeconds += 1;
+
+  if (totalSeconds === 60) {
+    totalMinutes += 1;
+    totalSeconds = 0;
+  }
+
+  if (totalMinutes === 60) {
+    totalHours += 1;
+    totalMinutes = 0;
+  }
+  return (focusedTime = {
+    hours: totalHours,
+    minutes: totalMinutes,
+    seconds: totalSeconds,
+  });
+};
+const setFocusText = function () {
+  let {
+    hours: totalHours,
+    minutes: totalMinutes,
+    seconds: totalSeconds,
+  } = focusedTime;
+  const totalH = document.querySelector(".focus-stopwatch .hour");
+  const totalM = document.querySelector(".focus-stopwatch .minute");
+  const totalS = document.querySelector(".focus-stopwatch .second");
+  totalH.textContent = totalHours < 10 ? `0${totalHours}` : totalHours;
+  totalM.textContent = totalMinutes < 10 ? `0${totalMinutes}` : totalMinutes;
+  totalS.textContent = totalSeconds < 10 ? `0${totalSeconds}` : totalSeconds;
+};
 
 // 모달 창 띄우기
 const addSubjectBtn = document.querySelector(".add-subject");
-const modal = document.querySelector("#modal");
+const addSubjectModal = modal.querySelector(".add-subject-modal");
 addSubjectBtn.addEventListener("click", () => {
   modal.style.display = "block";
+  addSubjectModal.classList.add("active");
 });
 
 // 모달 창에서 과목 추가
@@ -111,6 +190,7 @@ modalBtn.addEventListener("click", (e) => {
     modalInput.classList.add("no-name");
   } else {
     addSubject(subjectName);
+    modalInput.classList.remove("no-name");
   }
 });
 
@@ -122,6 +202,7 @@ const addSubject = (subjectName) => {
   </button>`;
   subjectList.append(subjectLi);
   modal.style.display = "none";
+  addSubjectModal.classList.remove("active");
   modalInput.value = "";
 
   // localStorage에 과목명으로 된 객체 추가
@@ -145,3 +226,12 @@ const setSubjectList = (loadSubjects) => {
     subjectList.append(subjectLi);
   }
 };
+
+// 집중 시간 모달 버튼 클릭하면 모달 닫기
+const focusModalCloseBtn = focusModal.querySelector("button");
+
+focusModalCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "none";
+  focusModal.classList.remove("active");
+});
