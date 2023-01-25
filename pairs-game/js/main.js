4f50487abd76fce035ca979c4c25173c6e37709e
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-01-25 16:43:23 +0900

    feat: 카드 짝맞추기 기능
    - 짝 맞춰진 카드는 클릭 못하도록
    - 짝 맞지 않는 카드는 다시 뒤집기

--- pairs-game/js/main.js
+++ pairs-game/js/main.js
@@ -31,11 +31,56 @@ function shuffle(cards) {
 shuffle(cards);
 
 // 카드 container에 세팅
-for (let i = 0; i <= shuffledCards.length - 1; i++) {
-  const cardEl = document.createElement("div");
-  cardEl.innerHTML = `<img src = '${require("/images/card.png")}' class='${
-    shuffledCards[i]
-  }' />`;
-  myCards.append(cardEl);
-  console.log(i);
+function setCards() {
+  for (let i = 0; i <= shuffledCards.length - 1; i++) {
+    const cardEl = document.createElement("div");
+    cardEl.classList.add("card");
+    cardEl.classList.add(shuffledCards[i]);
+    cardEl.dataset.item = shuffledCards[i];
+    cardEl.dataset.view = "card";
+    myCards.append(cardEl);
+  }
+}
+
+setCards();
+
+const myCardsEl = document.querySelectorAll(".card");
+let result = [];
+myCardsEl.forEach((item) => {
+  item.addEventListener("click", function () {
+    this.classList.add("flipped");
+    const clickedCard = this.dataset.item;
+    result.push(clickedCard);
+    matchCards(this);
+  });
+});
+
+function matchCards(el) {
+  setTimeout(() => {
+    if (result.length === 2) {
+      removeClass(el);
+    }
+  }, 1000);
+}
+
+function removeClass(el) {
+  const firstCard = el;
+  const secondCard = getSiblings(firstCard);
+  console.log(secondCard);
+  secondCard.forEach((item) => {
+    if (item.dataset.item == firstCard.dataset.item) {
+      firstCard.classList.add("same");
+      item.classList.add("same");
+      firstCard.disabled = true;
+      item.disabled = true;
+    }
+    firstCard.classList.remove("flipped");
+    item.classList.remove("flipped");
+  });
+  result = [];
+}
+function getSiblings(el) {
+  return Array.from(el.parentElement.children).filter(
+    (e) => e.classList.contains("flipped") && e !== el
+  );
 }
