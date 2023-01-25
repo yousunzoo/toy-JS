e7a863c04498bc34389d35a38f5732506412b2a4
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-01-25 18:09:40 +0900

    feat: 카드 비교하는 중에는 다른 카드 클릭 못하도록, bgm 추가

--- pairs-game/js/main.js
+++ pairs-game/js/main.js
@@ -1,3 +1,5 @@
+import bgm from "../music/game_bgm.mp3";
+
 const myCards = document.getElementById("container");
 const resultsArray = [];
 const images = [
@@ -12,10 +14,25 @@ const images = [
   "meat",
   "watermelon",
 ];
-
 // 이미지 배열 복제
 const cards = [...images, ...images];
+const readyEl = document.querySelector(".ready");
+const playBtn = readyEl.querySelector("button");
 
+// start 버튼 누르면 게임 시작 동시에 오디오 재생
+playBtn.addEventListener("click", function () {
+  // playBgm();
+  shuffle(cards);
+  setCards();
+  clickCards();
+  readyEl.style.display = "none";
+});
+function playBgm() {
+  var audio = new Audio(bgm);
+  audio.load();
+  audio.volume = 0.5;
+  audio.play();
+}
 // 카드 섞기
 
 let shuffledCards = [];
@@ -27,8 +44,6 @@ function shuffle(cards) {
   }
   return shuffledCards;
 }
-
-shuffle(cards);
 
 // 카드 container에 세팅
 function setCards() {
@@ -42,45 +57,58 @@ function setCards() {
   }
 }
 
-setCards();
+function clickCards() {
+  const myCardsEl = document.querySelectorAll(".card");
+  let clicked = [];
+  let pairedCards = 0;
+  let pass = true;
 
-const myCardsEl = document.querySelectorAll(".card");
-let result = [];
-myCardsEl.forEach((item) => {
-  item.addEventListener("click", function () {
-    this.classList.add("flipped");
-    const clickedCard = this.dataset.item;
-    result.push(clickedCard);
-    matchCards(this);
+  // 카드 클릭할 때마다 동작 수행
+  // 카드 두개 선택 시 일치하는지 비교
+  // 일치하지 않으면 다시 뒤집어 놓기
+  // 일치하면 카드에 "same" 클래스 추가 및 pairedCards에 +1 처리
+  // pairedCards이 10이 되면 게임 종료 및 걸린 시간 출력
+  myCardsEl.forEach((item) => {
+    item.addEventListener("click", async function () {
+      if (pass) {
+        pass = false;
+        this.classList.add("flipped");
+        const clickedCard = this.dataset.item;
+        clicked.push(clickedCard);
+        await matchCards(this);
+      }
+    });
   });
-});
 
-function matchCards(el) {
-  setTimeout(() => {
-    if (result.length === 2) {
-      removeClass(el);
-    }
-  }, 1000);
-}
+  async function matchCards(el) {
+    if (clicked.length === 2) {
+      let promise = new Promise((resolve) => {
+        setTimeout(() => resolve(adjustClasses(el)), 800);
+      });
 
-function removeClass(el) {
-  const firstCard = el;
-  const secondCard = getSiblings(firstCard);
-  console.log(secondCard);
-  secondCard.forEach((item) => {
-    if (item.dataset.item == firstCard.dataset.item) {
-      firstCard.classList.add("same");
-      item.classList.add("same");
-      firstCard.disabled = true;
-      item.disabled = true;
+      await promise;
     }
-    firstCard.classList.remove("flipped");
-    item.classList.remove("flipped");
-  });
-  result = [];
-}
-function getSiblings(el) {
-  return Array.from(el.parentElement.children).filter(
-    (e) => e.classList.contains("flipped") && e !== el
-  );
+    pass = true;
+  }
+
+  function adjustClasses(el) {
+    const firstCard = el;
+    const secondCard = getSiblings(firstCard);
+    secondCard.forEach((item) => {
+      if (item.dataset.item == firstCard.dataset.item) {
+        firstCard.classList.add("same");
+        item.classList.add("same");
+        pairedCards += 1;
+        console.log(pairedCards);
+      }
+      firstCard.classList.remove("flipped");
+      item.classList.remove("flipped");
+    });
+    clicked = [];
+  }
+  function getSiblings(el) {
+    return Array.from(el.parentElement.children).filter(
+      (e) => e.classList.contains("flipped") && e !== el
+    );
+  }
 }
